829712c1b29afd9d0cd4a926a3881bae447e417b
unknown <nikhilloya88@gmail.com>
2013-03-21 19:39:09 -0400

    Sample server client - not according to spec

---
+++ client.py
@@ -0,0 +1,28 @@
+import pickle
+import socket
+
+s = socket.socket()
+print s
+host = socket.gethostbyname(socket.gethostname()) #socket.gethostname() #6.14' #socket.gethostname()
+port = 7734
+#print host
+s.connect((host, port))
+
+#msg = pickle.loads(s.recv(1024))
+count = 0
+while(count <= 10000000):
+	if(count%100000==0):
+		print s.recv(1024)
+	count+=1
+	
+'''
+s1 = socket.socket()
+s1.bind((socket.gethostbyname(msg[0]) , msg[1]))
+s1.listen(5)
+
+while True:
+	client,addr = s1.accept()
+	client.send("Msg from Client")
+	client.close()
+#s.close()
+'''
---
+++ server.py
@@ -0,0 +1,53 @@
+import socket
+import pickle
+import threading
+
+
+
+class myThread (threading.Thread):
+	def __init__(self, threadID):
+		self.threadID = threadID
+		threading.Thread.__init__(self)
+	
+	def run(self, client, addr):
+		listen(client, addr)
+
+
+def main():
+	s = socket.socket()
+	print s
+	host = socket.gethostbyname(socket.gethostname())
+	port = 7734
+	#print host
+	s.bind((host, port))
+	prev = []
+	s.listen(5)
+	conn = 0
+	while True:
+		conn+=1
+		client, addr = s.accept()
+		thread = myThread(conn)
+		thread.run(client, addr)
+	
+	
+def listen(client, addr):
+	client.send("Thank you for connecting")
+	client.close()
+	'''
+	if conn%2==0:
+		print 'Connection from', addr
+		client.send(pickle.dumps(prev))
+	#print client.recv(1024)
+	#print s.accept()
+	else:
+		print addr[0], addr[1]
+		prev.append(addr[0])
+		prev.append(addr[1])
+		print client
+		print 'Connection from', addr
+		client.send(pickle.dumps(addr))
+	#client.close()
+	'''
+	
+	
+main()
